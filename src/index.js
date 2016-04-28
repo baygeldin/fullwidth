8fd190d6c7bbc1c33d79e59b2837470e2d9789e2
Alexander <a.baygeldin@gmail.com>
2016-04-28 16:03:02 +0300

    Switched to array lookup

--- src/index.js
+++ src/index.js
@@ -1,118 +1,18 @@
-// Search in array is O(n), hashmaps are O(1),
-// but char code evalution is still faster.
-// Hope it's not penny-wise and pound-foolish.
+import charCodes from './fullwidth'
 
-function convertChar(code) {
-    if (code >= 33 && code <= 126) {
-        return code + 65248;
-    } else if (code === 32 || (code >= 8192 && code <= 8201)) {
-        return 12288;
-    } else if (code === 163) {
-        return code + 65342;
-    } else if (code === 165) {
-        return code + 65344;
-    } else if (code === 183) {
-        return code + 65198;
-    } else if (code === 8361) {
-        return code + 57149;
-    } else if (code === 8373) {
-        return code + 57131;
-    } else if (code >= 65377 && code <= 65439) {
-        return halfWidthKanaCodes[code]
-    }
-}
-
-const halfWidthKanaMap = {
-    '｡': '。',
-    '｢': '「',
-    '｣': '」',
-    '､': '、',
-    '･': '・',
-    'ｦ': 'ヲ',
-    'ｧ': 'ァ',
-    'ｨ': 'ィ',
-    'ｩ': 'ゥ',
-    'ｪ': 'ェ',
-    'ｫ': 'ォ',
-    'ｬ': 'ャ',
-    'ｭ': 'ュ',
-    'ｮ': 'ョ',
-    'ｯ': 'ッ',
-    'ｰ': 'ー',
-    'ｱ': 'ア',
-    'ｲ': 'イ',
-    'ｳ': 'ウ',
-    'ｴ': 'エ',
-    'ｵ': 'オ',
-    'ｶ': 'カ',
-    'ｷ': 'キ',
-    'ｸ': 'ク',
-    'ｹ': 'ケ',
-    'ｺ': 'コ',
-    'ｻ': 'サ',
-    'ｼ': 'シ',
-    'ｽ': 'ス',
-    'ｾ': 'セ',
-    'ｿ': 'ソ',
-    'ﾀ': 'タ',
-    'ﾁ': 'チ',
-    'ﾂ': 'ツ',
-    'ﾃ': 'テ',
-    'ﾄ': 'ト',
-    'ﾅ': 'ナ',
-    'ﾆ': 'ニ',
-    'ﾇ': 'ヌ',
-    'ﾈ': 'ネ',
-    'ﾉ': 'ノ',
-    'ﾊ': 'ハ',
-    'ﾋ': 'ヒ',
-    'ﾌ': 'フ',
-    'ﾍ': 'ヘ',
-    'ﾎ': 'ホ',
-    'ﾏ': 'マ',
-    'ﾐ': 'ミ',
-    'ﾑ': 'ム',
-    'ﾒ': 'メ',
-    'ﾓ': 'モ',
-    'ﾔ': 'ヤ',
-    'ﾕ': 'ユ',
-    'ﾖ': 'ヨ',
-    'ﾗ': 'ラ',
-    'ﾘ': 'リ',
-    'ﾙ': 'ル',
-    'ﾚ': 'レ',
-    'ﾛ': 'ロ',
-    'ﾜ': 'ワ',
-    'ﾝ': 'ン',
-    'ﾞ': '゛',
-    'ﾟ': '゜'
-}
-
-const halfWidthKanaCodes = Object
-    .keys(halfWidthKanaMap)
+const codes = Object
+    .keys(charCodes)
     .reduce((map, key) => {
-        map[key.charCodeAt(0)] = halfWidthKanaMap[key].charCodeAt(0)
+        map[key.charCodeAt(0)] = charCodes[key].charCodeAt(0)
         return map
     }, {})
 
-function hasFullwidth(code) {
-    // 33 - !, 126 - ~, 163 - £, 165 - ¥,
-    // 183 - ·, 8361 - ₩, 8373 - ₵
-    return code >= 32 && code <= 126 ||
-        (code >= 8192 && code <= 8201) ||
-        code === 163 || code === 165 ||
-        code === 183 || code === 8361 ||
-        code === 8373 ||
-        (code >= 65377 && code <= 65439);
-}
-
 export default str => {
-    let res = ''
+    let res = '';
 
     for (let ch of str) {
-        let code = ch.charCodeAt(0);
-        res += hasFullwidth(code) ?
-            String.fromCharCode(convertChar(code)) : ch;
+        let code = codes[ch.charCodeAt(0)];
+        res += code ? String.fromCharCode(code) : ch;
     }
 
     return res;
